e197dab155933b66e6fdb9606b0057f3ece3265b
Brynley Llewellyn-Roux <blynblack@gmail.com>
2024-08-23 15:08:05 +1000

    fix: cleaned up `flake.nix` outputs

--- flake.nix
+++ flake.nix
@@ -4,19 +4,17 @@
   inputs = {
     nixpkgs.url =
       "github:NixOS/nixpkgs/3f33387a5c85d94b305062a4f97d5b2899094efa";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        packages = pkgs.callPackage ./packages.nix { };
-        overlay = final: prev: packages;
-      in {
-        packages = packages;
-        legacyPackages = pkgs.extend overlay;
-        lib = nixpkgs.lib;
-      });
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      packages = final: prev: (pkgs.callPackage ./packages.nix { });
+      nixpkgs-matrix = pkgs.extend packages;
+    in {
+      legacyPackages.${system} = nixpkgs-matrix;
+      lib = nixpkgs.lib;
+    };
 }
 
--- packages.nix
+++ packages.nix
@@ -1,3 +1,6 @@
 { system, lib }:
 
-lib.makeExtensible (self: { })
+lib.makeExtensible (self: {
+  polykey-cli = (builtins.getFlake
+    "github:MatrixAI/Polykey-CLI/6d7224dddbb7356e74acf4f117892766a73f9cc9").packages.${system}.default;
+})
