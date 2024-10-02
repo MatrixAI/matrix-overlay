e83f34c40090386011e5cc28aac49f806a9b71a6
Brynley Llewellyn-Roux <blynblack@gmail.com>
2024-10-02 12:36:01 +1000

    deps: update `Polykey-CLI` to `219b965`

--- packages.nix
+++ packages.nix
@@ -2,5 +2,5 @@
 
 lib.makeExtensible (self: {
   polykey-cli = (builtins.getFlake
-    "github:MatrixAI/Polykey-CLI/40fb687c9209408965043c2b322f3a2643e275bc").packages.${system}.default;
+    "github:MatrixAI/Polykey-CLI/219b965e47c70422f840e537b74d8937e9a046b1").packages.${system}.default;
 })
