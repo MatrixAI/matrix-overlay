b1c7fcbe9ba2e97e1aff9dc71e4c8fcf020a4d65
Brian Botha <brian.botha@matrix.ai>
2024-12-10 11:25:14 +1100

    deps: updated `Polykey-CLI` to `e452247`

--- packages.nix
+++ packages.nix
@@ -2,5 +2,5 @@
 
 lib.makeExtensible (self: {
   polykey-cli = (builtins.getFlake
-    "github:MatrixAI/Polykey-CLI/73e25d8aa49873f7b6ea1be38f37408649c6c24e").packages.${system}.default;
+    "github:MatrixAI/Polykey-CLI/e4522475e79957aaef16dca39e55024d1a508ce6").packages.${system}.default;
 })
